bda2317539bc6bed1c1356f5cc2268a9f2394746
tantaroth <tantaroth@gmail.com>
2016-07-11 19:17:36 -0500

    modulo de seguridad inicializado

---
+++ dev/src/app/safe/safe.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-safe',
+  templateUrl: 'safe.component.html',
+  styleUrls: ['safe.component.css']
+})
+export class SafeComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
