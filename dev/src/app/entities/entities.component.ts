94a5bf68643c97aef7708d6fd72623fd7a5a8c2f
tantaroth <tantaroth@gmail.com>
2016-07-12 19:11:20 -0500

    init entity module

---
+++ dev/src/app/entities/entities.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-entities',
+  templateUrl: 'entities.component.html',
+  styleUrls: ['entities.component.css']
+})
+export class EntitiesComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
