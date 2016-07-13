8cc263334b85d22b3b0867091f8366d8ceca0903
tantaroth <tantaroth@gmail.com>
2016-07-12 19:11:59 -0500

    init invoice module

---
+++ dev/src/app/invoices/invoices.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-invoices',
+  templateUrl: 'invoices.component.html',
+  styleUrls: ['invoices.component.css']
+})
+export class InvoicesComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
